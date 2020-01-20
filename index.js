e3461e1f55fbd9c4b7449259fd7a224dbeb2fade
Matthew Gatland <mgatland@gmail.com>
2020-01-21 09:53:58 +1300

    no more semis

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //set up
-let express = require('express');
-let app = express();
+let express = require('express')
+let app = express()
 
 //If a client asks for a file,
 //look in the public folder. If it's there, give it to them.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
-app.listen(3000);
-console.log("Hi! I am listening at http://localhost:3000");
+app.listen(3000)
+console.log("Hi! I am listening at http://localhost:3000")
